e489812cf3a37079c1078df1c4e46f81cdd96bb5
Pramod CR <pramodcr28@gmail.com>
2025-09-09 18:59:33 +0530

    re routing user to login page  if token expired

--- src/app/core/interceptor/auth-cookie-interceptor.interceptor.ts
+++ src/app/core/interceptor/auth-cookie-interceptor.interceptor.ts
@@ -5,6 +5,17 @@ import { switchMap, take, tap, throwError } from 'rxjs';
 import { getToken } from '../store/user-profile/user-profile.selectors';
 import { Router } from '@angular/router';
 
+function isTokenExpired(token: string): boolean {
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const exp = payload.exp;
+    if (!exp) return true;
+    const now = Math.floor(Date.now() / 1000);
+    return exp < now;
+  } catch (e) {
+    return true;
+  }
+}
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const store = inject(Store);
   const router = inject(Router);
@@ -13,12 +24,19 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     take(1),
     switchMap(token => {
       let modifiedReq = req;
-      if (token &&  !req.url.includes('/generate-timetable')) {
-        modifiedReq = req.clone({
+      if (token) {
+
+        if(isTokenExpired(token)){
+           router.navigate(['/auth/login']);
+           throwError(() => new Error('No token, redirecting to login'));
+        }
+        if(!req.url.includes('/generate-timetable')){
+         modifiedReq = req.clone({
           setHeaders: {
             Authorization: `Bearer ${token}`
           }
         });
+        }
       }else{
         if (!req.url.includes('/api/authenticate')) {
           router.navigate(['/auth/login']);
