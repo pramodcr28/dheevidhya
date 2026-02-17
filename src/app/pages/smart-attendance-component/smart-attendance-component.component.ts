11b0570353542dfe3448d64da85b81adc61a139e
Pramod CR <pramodcr28@gmail.com>
2026-02-17 22:45:52 +0530

    attendence related code

--- src/app/pages/smart-attendance-component/smart-attendance-component.component.ts
+++ src/app/pages/smart-attendance-component/smart-attendance-component.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Tabs, TabsModule } from 'primeng/tabs';
+import { TabsModule } from 'primeng/tabs';
 import { DetailedReportsComponent } from './detailed-reports/detailed-reports.component';
 import { TakeAttendenceComponent } from './take-attendence/take-attendence.component';
 
 @Component({
     selector: 'app-smart-attendance',
     standalone: true,
-    imports: [CommonModule, Tabs, TakeAttendenceComponent, DetailedReportsComponent, TabsModule],
+    imports: [CommonModule, TakeAttendenceComponent, DetailedReportsComponent, TabsModule],
     templateUrl: './smart-attendance-component.component.html'
 })
 export class SmartAttendanceComponent {
-    activeTab: string = '0';
+    // activeTab: string = '0';
+    activeView: 'take' | 'report' = 'take';
+
+    today: Date = new Date();
 }
