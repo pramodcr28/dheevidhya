c4b546f8a78ef0945602496161b8582f94519185
Pramod CR <pramodcr28@gmail.com>
2026-03-26 22:56:00 +0530

    capacitor related  code

---
+++ android/app/src/main/java/com/v3tech/dheevidhya/MainActivity.java
@@ -0,0 +1,5 @@
+package com.v3tech.dheevidhya;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'com.v3tech.dheevidhya',
+    appName: 'dheevidhya',
+    webDir: 'dist/dheevidhya/browser'
+};
+
+export default config;
